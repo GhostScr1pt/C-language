cade2aca24221c64e732bdb4c214b25b0f7bb29f
GhostScr1pt <123635909+GhostScr1pt@users.noreply.github.com>
2023-01-30 20:20:26 +0800

    Add files via upload

---
+++ fun_time.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+   int num;
+   char r[25];
+   system("color 0a"); 
+   system("cls");
+   printf(">>> ");
+   gets(r); 
+   if(strcmp(r,"funtime")==0)
+     system("cls");
+     for(num=1;num<=100000000;num++)
+     {
+       printf(" 1001 1010 1100 1101");
+     }
+}   

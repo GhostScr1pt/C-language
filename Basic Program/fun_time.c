e8579cd68f654b2f96a198a0884a90e7362e137d
丂ʜᴀᴅᴏᴡ尺ᴇᴀᴘᴇ尺 <123635909+ShadowR3aper@users.noreply.github.com>
2023-06-14 09:31:26 +0800

    Add files via upload

---
+++ Basic Program/fun_time.c
@@ -0,0 +1,24 @@
+/* * *
+ CODED BY   : 丂ʜᴀᴅᴏᴡ尺ᴇᴀᴘᴇ尺
+ FROM       : 𝐂𝐓 𝐈𝐍𝐂𝐎𝐑𝐏𝐎𝐑𝐀𝐓𝐄𝐃
+ PATCH DATE : ４/８/２３
+ * * */
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+   int num;
+   char r[25];
+   system("color 0a"); 
+   system("cls");
+   printf(">>> ");
+   gets(r); 
+   if(strcmp(r,"funtime")==0)
+     system("cls");
+     for(num=1;num<=100000000;num++)
+     {
+       printf(" 1001 1010 1100 1101");
+     }
+}   
